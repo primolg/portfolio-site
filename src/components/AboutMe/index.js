f28c69dcb9b447e900f607ba344c5311c4177798
primogill <primoledeboergill@gmail.com>
2023-02-21 20:19:36 -0500

    updated bio

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -19,7 +19,7 @@ function AboutMe() {
           <img src={primoPhone} alt="primo gill portrait" />
         </div>
           <p id="about-me-text">
-            I am a recent graduate from both Fullstack Academy’s web development program, and CCNY, where I completed a bachelor's degree in Jewish Studies, with a focus in graphic design. I’m a dual citizen who’s moved back and fourth between the Netherlands and the United States throughout my life, and am currently residing in the Netherlands.
+            I am a recent graduate from both Fullstack Academy’s web development program, and the City College of New York, where I completed a bachelor's degree in Jewish Studies, with a focus in graphic design. I’m a dual citizen who’s moved back and fourth between the Netherlands and the United States throughout my life, and am currently residing in New York City.
             <br></br>
                 Next to web development I have a passion for graphic design and making music. 
           </p>
