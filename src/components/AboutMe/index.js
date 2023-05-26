5be4db847c21294abd6b687e3256c33f6e4e87dd
primogill <primoledeboergill@gmail.com>
2023-05-26 01:56:50 -0400

    cleanup updates

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -19,9 +19,12 @@ function AboutMe() {
           <img src={primoPhone} alt="primo gill portrait" />
         </div>
           <p id="about-me-text">
-            I am a recent graduate from both Fullstack Academy’s web development program, and the City College of New York, where I completed a bachelor's degree in Jewish Studies, with a focus in graphic design. I’m a dual citizen who’s moved back and fourth between the Netherlands and the United States throughout my life, and am currently residing in New York City.
-            <br></br>
-                Next to web development I have a passion for graphic design and making music. 
+            <div>
+              I am a recent graduate from both Fullstack Academy’s web development program, and the City College of New York, where I completed a bachelor's degree in Religious Studies and graphic design. I am a dual citizen who’s moved back and fourth between the Netherlands and the United States throughout my life, and am currently residing in New York City.
+            </div>
+            <div>
+              Alongside web development and design I have a passion for music production and public transit.
+            </div>
           </p>
       </div>
     </div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -18,60 +18,52 @@ function Projects() {
       <div id="projects">
         {/* NS MEETUP */}
           <div className="project">
-            <div>
+            <div className="project-titles">
               <h3>NS Meetup</h3>
-              <h4>public transit meetup app</h4>
+              <h4>Public transit meetup app</h4>
             </div>
-            <div>-uses third party API's to find trips and their related locations</div>
-            <div>-developed using React, Axios, (Less) CSS, & other libraries</div>
+            <div>∙ Uses third party API's to find trips and their related locations</div>
+            <div>∙ Developed using React, Axios, (Less) CSS, & other libraries</div>
             <div>
-              app: <a href="https://nsmeetup.netlify.app/">https://nsmeetup.netlify.app </a>(mobile recommended)
-            </div>
-            <div>
-              github: <a href="https://github.com/primolg/NS-meetup"> https://github.com/primolg/NS-meetup</a>
+            <a href="https://nsmeetup.netlify.app/">App (mobile)</a>
+            <a href="https://github.com/primolg/NS-meetup">Github</a>
             </div>
             <img src={NSshow} alt="NS-Meetup app display" />
           </div>
         {/* BOOK BEASTS */}
           <div className="project">
-            <div>
+            <div className="project-titles">
               <h3>Book Beasts</h3>
-              <h4>content management/writing tool</h4>
+              <h4>Content management/writing tool</h4>
             </div>
-            <div>-application for students (aged 5-12) to write books, and instructors to manage and review their students' work</div>
-            <div>-developed using React, Redux, PSQL, Axios, (Less) CSS, & other libraries</div>
-            <div>-my responsibilities were developing the student portal & writing tool</div>
+            <div>∙ An application for students (aged 5-12) to write books, and instructors to manage and review their students' work</div>
+            <div>∙ Developed using React, Redux, PSQL, Axios, (Less) CSS, & other libraries</div>
+            <div>∙ My responsibilities included developing the student portal & writing tool</div>
             <div>
-              app: <a href="https://book-beasts.herokuapp.com/">https://book-beasts.herokuapp.com </a>
-            </div>
-            <div>
-              github: <a href="https://github.com/primolg/Book-Beasts">https://github.com/primolg/Book-Beasts</a>
+            <a href="https://github.com/primolg/Book-Beasts">Github</a>
             </div>
           </div>
         {/* LoL Cards */}
         <div className="project">
-            <div>
+            <div className="project-titles">
               <h3>LoL Cards</h3>
-              <h4>e-commerce store</h4>
+              <h4>E-commerce store</h4>
             </div>
-            <div>-e-commerce store to buy league of legend cards (not a real product)</div>
-            <div>-developed using React, Redux, PSQL, Axios, (Less) CSS, & other libraries</div>
-            <div>-my responsibilities included developing cart/guest cart feature, styling, and project planning</div>
+            <div>∙ An E-commerce store to buy league of legend cards (not a real product)</div>
+            <div>∙ Developed using React, Redux, PSQL, Axios, (Less) CSS, & other libraries</div>
+            <div>∙ My responsibilities included developing cart/guest cart feature, styling, and project planning</div>
             <div>
-              app: <a href="http://ecommercelolcards.herokuapp.com/">http://ecommercelolcards.herokuapp.com/ </a>
-            </div>
-            <div>
-              github: <a href="https://github.com/primolg/League-of-Legends-Cards">https://github.com/primolg/League-of-Legends-Cards</a>
+              <a href="https://github.com/primolg/League-of-Legends-Cards">Github</a>
             </div>
           </div>
         {/* PORTFOLIO SITE */}
         <div className="project">
-            <div>
+            <div className="project-titles">
               <h3>Portfolio site</h3>
             </div>
-            <div>-developed using React, (Less) CSS, & other libraries</div>
+            <div>∙ developed using React, (Less) CSS, & other libraries</div>
             <div>
-              github: <a href="https://github.com/primolg/portfolio-site">https://github.com/primolg/portfolio-site</a>
+              <a href="https://github.com/primolg/portfolio-site">Github</a>
             </div>
           </div>
       </div>
